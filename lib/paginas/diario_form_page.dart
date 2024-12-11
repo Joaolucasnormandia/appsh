01c96af874c96a0e8b92dfad030c0ff821d045f3
ALUNO <ALUNO@senaiescola.fsa>
2024-12-11 11:20:56 -0300

    diario

---
+++ lib/paginas/diario_form_page.dart
@@ -0,0 +1,186 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class DiarioFormPage extends StatefulWidget {
+  final String dia;
+  final String? anotacaoExistente; 
+
+  DiarioFormPage({required this.dia, this.anotacaoExistente});
+
+  @override
+  _DiarioFormPageState createState() => _DiarioFormPageState();
+}
+
+class _DiarioFormPageState extends State<DiarioFormPage> {
+  final TextEditingController _humorController = TextEditingController();
+  final TextEditingController _atividadesController = TextEditingController();
+  final TextEditingController _alimentacaoController = TextEditingController();
+  final TextEditingController _notasController = TextEditingController();
+  final TextEditingController _aguaController = TextEditingController(); 
+
+  @override
+  void initState() {
+    super.initState();
+    _carregarDados(); 
+  }
+
+  void _carregarDados() async {
+    final prefs = await SharedPreferences.getInstance();
+
+    setState(() {
+      _humorController.text = prefs.getString('humor_${widget.dia}') ?? '';
+      _atividadesController.text = prefs.getString('atividades_${widget.dia}') ?? '';
+      _alimentacaoController.text = prefs.getString('alimentacao_${widget.dia}') ?? '';
+      _notasController.text = prefs.getString('notas_${widget.dia}') ?? '';
+      _aguaController.text = prefs.getString('agua_${widget.dia}') ?? ''; 
+    });
+  }
+
+ void _saveData() async {
+  if (_humorController.text.isEmpty ||
+      _atividadesController.text.isEmpty ||
+      _alimentacaoController.text.isEmpty ||
+      _aguaController.text.isEmpty) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(content: Text('Por favor, preencha todos os campos antes de salvar.')),
+    );
+    return; 
+  }
+
+  if (double.tryParse(_aguaController.text) == null) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(content: Text('Por favor, insira um valor válido para a quantidade de água.')),
+    );
+    return;
+  }
+
+  final prefs = await SharedPreferences.getInstance();
+  await prefs.setString('humor_${widget.dia}', _humorController.text);
+  await prefs.setString('atividades_${widget.dia}', _atividadesController.text);
+  await prefs.setString('alimentacao_${widget.dia}', _alimentacaoController.text);
+  await prefs.setString('notas_${widget.dia}', _notasController.text);
+  await prefs.setString('agua_${widget.dia}', _aguaController.text); 
+
+  ScaffoldMessenger.of(context).showSnackBar(
+    SnackBar(content: Text('Anotação salva!')),
+  );
+
+  Navigator.pop(context, true);
+}
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+  title: Text(
+    widget.dia,
+    style: TextStyle(
+      color: Colors.white, // Mudando a cor do título para branco
+    ),
+  ),
+  backgroundColor: const Color.fromARGB(255, 0, 0, 0),
+   iconTheme: IconThemeData(
+    color: Colors.white, // Mudando a cor da seta de voltar para branco
+  ),
+),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Como você se sentiu hoje?',
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+              ),
+              SizedBox(height: 10),
+              TextFormField(
+                controller: _humorController,
+                decoration: InputDecoration(
+                  hintText: 'Descreva seu humor...',
+                  border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+                ),
+                maxLines: 3,
+              ),
+              SizedBox(height: 20),
+              Text(
+                'Atividades realizadas:',
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+              ),
+              SizedBox(height: 10),
+              TextFormField(
+                controller: _atividadesController,
+                decoration: InputDecoration(
+                  hintText: 'Descreva suas atividades...',
+                  border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+                ),
+                maxLines: 3,
+              ),
+              SizedBox(height: 20),
+              Text(
+                'Alimentação:',
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+              ),
+              SizedBox(height: 10),
+              TextFormField(
+                controller: _alimentacaoController,
+                decoration: InputDecoration(
+                  hintText: 'O que você comeu hoje?',
+                  border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+                ),
+                maxLines: 3,
+              ),
+              SizedBox(height: 20),
+              Text(
+                'Quantos litros de água você bebeu hoje?',
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+              ),
+              SizedBox(height: 10),
+              TextFormField(
+                controller: _aguaController,
+                decoration: InputDecoration(
+                  hintText: 'Informe a quantidade em Litros   ...',
+                  border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+                ),
+                keyboardType: TextInputType.number,
+              ),
+              SizedBox(height: 20),
+              Text(
+                'Notas:',
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+              ),
+              SizedBox(height: 10),
+              TextFormField(
+                controller: _notasController,
+                decoration: InputDecoration(
+                  hintText: 'Adicione observações extras...',
+                  border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+                ),
+                maxLines: 3,
+              ),
+              SizedBox(height: 30),
+              Center(
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color.fromARGB(255, 0, 0, 0),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    padding: EdgeInsets.symmetric(horizontal: 40, vertical: 15),
+                  ),
+                   child: Text(
+                     'Salvar',
+                    style: TextStyle(
+                  color: Colors.white, // Mudando a cor do texto "Salvar" para branco
+                  ),
+                   ),
+                  onPressed: _saveData, 
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/paginas/diario_list_page.dart
@@ -0,0 +1,185 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'diario_form_page.dart';
+
+class DiarioListPage extends StatefulWidget {
+  @override
+  _DiarioListPageState createState() => _DiarioListPageState();
+}
+
+class _DiarioListPageState extends State<DiarioListPage> {
+  List<String> dias = ['Dia 1', 'Dia 2', 'Dia 3', 'Dia 4', 'Dia 5'];
+  List<String> diasFiltrados = []; 
+  Map<String, String> anotacoes = {};
+
+  @override
+  void initState() {
+    super.initState();
+    diasFiltrados = dias; 
+    _loadData(); 
+  }
+
+  void _loadData() async {
+    final prefs = await SharedPreferences.getInstance();
+    Map<String, String> newAnotacoes = {};
+
+    for (var dia in dias) {
+      String? savedHumor = prefs.getString('humor_$dia');
+      newAnotacoes[dia] = savedHumor ?? 'Sem anotações';
+    }
+
+    setState(() {
+      anotacoes = newAnotacoes;
+    });
+  }
+
+  void _filtrarDias(String query) {
+    setState(() {
+      diasFiltrados = dias.where((dia) {
+        final anotacao = anotacoes[dia]?.toLowerCase() ?? '';
+        return dia.toLowerCase().contains(query.toLowerCase()) ||
+               anotacao.contains(query.toLowerCase());
+      }).toList();
+    });
+  }
+
+  void _removerRegistro(String dia) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.remove('humor_$dia');
+    await prefs.remove('atividades_$dia');
+    await prefs.remove('alimentacao_$dia');
+    await prefs.remove('notas_$dia');
+    await prefs.remove('agua_$dia'); 
+
+    setState(() {
+      dias.remove(dia); 
+      diasFiltrados = dias; 
+      anotacoes.remove(dia); 
+    });
+
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(content: Text('Registro "$dia" removido com sucesso.')),
+    );
+  }
+
+  void _adicionarNovoDia() {
+    setState(() {
+      int novoDia = dias.isNotEmpty ? dias.length + 1 : 1; 
+      String novoDiaStr = 'Dia $novoDia';
+      dias.add(novoDiaStr); 
+      diasFiltrados = dias; 
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Meu Diário',
+          style: TextStyle(
+            color: Colors.white, 
+            fontSize: 22, 
+            fontWeight: FontWeight.w600, 
+          ),
+        ),
+        backgroundColor: const Color.fromARGB(255, 0, 0, 0),
+        elevation: 4, 
+        iconTheme: IconThemeData(
+          color: Colors.white,
+        ),
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: TextField(
+              onChanged: _filtrarDias,
+              decoration: InputDecoration(
+                labelText: 'Pesquisar...',
+                labelStyle: TextStyle(color: Colors.black, fontWeight: FontWeight.w400),
+                prefixIcon: Icon(Icons.search, color: Colors.black),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(20), 
+                  borderSide: BorderSide(color: Colors.black26), 
+                ),
+                filled: true, 
+                fillColor: Colors.white, 
+              ),
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: diasFiltrados.length,
+              itemBuilder: (context, index) {
+                final dia = diasFiltrados[index];
+                return Card(
+                  margin: EdgeInsets.all(12.0),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15), 
+                  ),
+                  elevation: 5, 
+                  child: ListTile(
+                    contentPadding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 15.0),
+                    leading: CircleAvatar(
+                      backgroundColor: const Color.fromARGB(255, 0, 0, 0),
+                      child: Icon(Icons.calendar_today, color: const Color.fromARGB(255, 255, 245, 233)),
+                    ),
+                    title: Text(
+                      dia,
+                      style: TextStyle(fontWeight: FontWeight.w600, fontSize: 18),
+                    ),
+                    subtitle: Text(
+                      anotacoes[dia] ?? 'Sem anotações',
+                      style: TextStyle(fontSize: 14),
+                    ),
+                    trailing: IconButton(
+                      icon: Icon(Icons.delete, color: Colors.red),
+                      onPressed: () => _removerRegistro(dia),
+                    ),
+                    onTap: () async {
+                      final bool? updated = await Navigator.push<bool>(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => DiarioFormPage(
+                            dia: dia,
+                            anotacaoExistente: anotacoes[dia],
+                          ),
+                        ),
+                      );
+                      if (updated == true) {
+                        _loadData();
+                      }
+                    },
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: const Color.fromARGB(255, 0, 0, 0),
+        child: Icon(
+          Icons.add,
+          color: const Color.fromARGB(255, 255, 245, 233),
+        ),
+        onPressed: () async {
+          _adicionarNovoDia();
+          final bool? updated = await Navigator.push<bool>(
+            context,
+            MaterialPageRoute(
+              builder: (context) => DiarioFormPage(
+                dia: 'Dia ${dias.length}',
+                anotacaoExistente: null,
+              ),
+            ),
+          );
+          if (updated == true) {
+            _loadData(); 
+          }
+        },
+      ),
+    );
+  }
+}
